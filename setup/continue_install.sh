d79eef8c51662b3164a94dfed01488c66b849a89
Jerry Hopper <hopper.jerry@gmail.com>
2013-10-04 20:31:20 +0200

    x

--- setup/continue_install.sh
+++ setup/continue_install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 echo "install.sh"
-sudo apt-get -y install build-essential pkg-config python-mysqldb libpcre3-dev
+sudo apt-get -y install build-essential pkg-config python-mysqldb libpcre3-dev zlib1g-dev squid
+sudo service squid3 stop
 
 cd /usr/local/surfwijzer/setup
 tar -xvf dansguardian-2.12.0.3.tar.bz2
+cd /usr/local/surfwijzer/setup/dansguardian-2.12.0.3
 
+sudo ./configure  --prefix=/usr/local/surfwijzer/node --enable-ntlm=yes --enable-avastd=yes
+sudo make
+sudo make install
+sudo make clean
 
