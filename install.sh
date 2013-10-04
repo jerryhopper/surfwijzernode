e0f554e36cf723058da5ebb2746fdf5617b2de8c
Jerry Hopper <hopper.jerry@gmail.com>
2013-10-04 16:57:42 +0200

    x

--- install.sh
+++ install.sh
@@ -5,20 +5,22 @@ if [ "$OSVERSION" != "Ubuntu 12.04.3 LTS" ]; then
 	echo "no good"
 else
 	Directory="/usr/local/surfwijzer"
-	if [ -d "$Directory" ]
+	if [ -d "/usr/local/surfwijzer" ]
 	then
+		echo "EXISTS"
 		echo "UPDATING LOCAL CODE WITH LATEST VERSION"
-		cd $Directory
-		git pull
-		sudo sh /usr/local/surfwijzer/setup/install.sh
-		exit 0
-	else
-		echo "UPDATING LOCAL CODE WITH LATEST VERSION"
-		sudo mkdir $Directory
 		sudo apt-get install git
 		cd $Directory
-		git clone git://github.com/jerryhopper/surfwijzernode $Directory
+		git pull
 		sudo sh /usr/local/surfwijzer/setup/update.sh
+		exit 0
+	else
+		echo "NOTHING"
+		echo "INSTALLING LOCAL CODE WITH LATEST VERSION"
+		sudo mkdir $Directory
+		cd $Directory
+		git clone git://github.com/jerryhopper/surfwijzernode $Directory
+		sudo sh /usr/local/surfwijzer/setup/install.sh
 		exit 0
 	fi	
 	
